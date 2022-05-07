f09bb9af7fa47ec249f5b11d0532eede03ff2dad
parthpandyappp <parthpandyappp@gmail.com>
2022-05-07 11:55:19 +0530

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extent: {
+      colors: {
+        body: "#FFFFFE",
+        heading: "#094067",
+        paragraph: "#5F6C7B",
+        button: "#3DA9FC",
+        buttonText: "#FFFFFE",
+      },
+    }
+  },
+  plugins: [],
+};
