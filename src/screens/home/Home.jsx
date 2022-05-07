c7a0bf37b4c2880a161c0fc1666654bca95e2566
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-05-07 13:47:53 +0530

    fix: tailwind config typo

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 const Home = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <button className="bg-black text-white flex items-center gap-2 px-2 py-4 rounded font-medium   hover:opacity-90 ">
+    <div className="w-full h-screen flex justify-center items-center ">
+      <button className="bg-black text-white flex items-center gap-2 px-2 py-4 rounded font-medium   hover:opacity-90">
         <AiFillGithub /> Sign in with Github
       </button>
     </div>
