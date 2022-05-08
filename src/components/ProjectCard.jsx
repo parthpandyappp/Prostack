c2a9066ac36ae64d24c91473120631ebbd321d2f
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-05-09 03:59:11 +0530

    feat: delete user projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,8 @@
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
+import { useAuth } from "../context";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 function ProjectCard({ projectInfo }) {
   const {
@@ -9,11 +13,36 @@ function ProjectCard({ projectInfo }) {
     description,
     techStackArray,
   } = projectInfo;
+
+  const { currentUser, getUser } = useAuth();
+
+  const deleteHandler = async (e) => {
+    try {
+      const docRef = doc(db, "users", currentUser.uid);
+      const projectArr = currentUser.projects;
+      const filteredProjectArr = projectArr.filter(
+        (project) => project.projectName !== projectName
+      );
+      await updateDoc(docRef, {
+        projects: filteredProjectArr,
+      });
+      await getUser(currentUser.uid);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-8 w-10/12 m-auto shadow-lg flex flex-col gap-2 rounded-lg">
       <div className="flex justify-between items-center gap-4 flex-wrap">
         <div className="flex items-center gap-2">
           <p className="text-3xl font-semibold text-heading">{projectName}</p>
+          <div>
+            <AiFillDelete
+              className="text-red-700 text-2xl cursor-pointer"
+              onClick={deleteHandler}
+            />
+          </div>
         </div>
         <p className="text-button text-xs font-bold border-2 border-button p-2 rounded-full">
           {category.toUpperCase()}
