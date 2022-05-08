4782008c05b5e0aea6e8ee42c1bddcac1d4da38b
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-05-09 01:53:48 +0530

    feat: added toggle switch

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,10 +1,39 @@
 import "../styles/toggleswitch.css";
+import { updateDoc, doc } from "firebase/firestore";
+import { useAuth } from "../context";
+import { db } from "../firebase/firebase";
+import { useState } from "react";
 
 function ToggleSwitch() {
+  const [collabToggle, setCollabToggle] = useState(false);
+  const {
+    currentUser: { uid, isOpenForCollab },
+    getUser,
+  } = useAuth();
+  const docRef = doc(db, "users", uid);
+
+  const toggleHandler = async (e) => {
+    try {
+      await updateDoc(docRef, {
+        isOpenForCollab: collabToggle,
+      });
+      await getUser(uid);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="toggle-switch flex gap-4 items-center">
       <label className="switch">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={isOpenForCollab}
+          onChange={() => {
+            setCollabToggle((collabToggle) => !collabToggle);
+            toggleHandler();
+          }}
+        />
         <span className="slider round"></span>
       </label>
       <label>Open for Collaborations</label>
