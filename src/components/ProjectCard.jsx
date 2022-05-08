cbe60227c737fe66b6bc504b7828e2f9516f9e37
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-05-09 01:53:19 +0530

    style: updated user profile styles

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,30 +1,42 @@
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 function ProjectCard({ projectInfo }) {
-  const { title, tags, githubLink, description, techStack } = projectInfo;
+  const {
+    projectName,
+    gitHubLink,
+    category,
+    tagArray,
+    description,
+    techStackArray,
+  } = projectInfo;
   return (
-    <div className="mx-24 mt-5 flex gap-10 px-10 py-5 h-56 shadow-lg rounded-lg">
-      <div className="project-details flex flex-col gap-3 w-full">
-        <h1 className="text-2xl font-semibold text-heading">{title}</h1>
-        <div className="flex gap-1 flex-wrap">
-          {tags.map((tag) => {
-            return (
-              <span
-                key={tag}
-                className="bg-button text-white font-medium py-[2px] px-4 rounded-full"
-              >
-                {tag}
-              </span>
-            );
-          })}
+    <div className="p-8 w-10/12 m-auto shadow-lg flex flex-col gap-2 rounded-lg">
+      <div className="flex justify-between items-center gap-4 flex-wrap">
+        <div className="flex items-center gap-2">
+          <p className="text-3xl font-semibold text-heading">{projectName}</p>
         </div>
-        <p className="text-md text-gray-600">{description}</p>
-        <div className="flex gap-3 text-heading font-medium">
-          Tech Stack: {techStack.join(", ")}
-        </div>
+        <p className="text-button text-xs font-bold border-2 border-button p-2 rounded-full">
+          {category.toUpperCase()}
+        </p>
+      </div>
+      <div className="flex gap-1 flex-wrap">
+        {tagArray.map((tag) => {
+          return (
+            <span
+              key={tag}
+              className="bg-button text-white font-medium py-[2px] px-4 rounded-full"
+            >
+              {tag}
+            </span>
+          );
+        })}
+      </div>
+      <p className="text-paragraph">{description}</p>
+      <div className="text-heading font-medium">
+        Tech Stack: {techStackArray.join(", ")}
       </div>
       <a
-        href={githubLink}
+        href={gitHubLink}
         className="self-end h-8 w-10 bg-button rounded-full flex justify-center items-center"
       >
         <MdOutlineKeyboardArrowRight className="text-2xl font-bold text-buttonText" />
