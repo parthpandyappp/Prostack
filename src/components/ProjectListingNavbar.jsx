bd562f95a4b8fd21d49e9b31beec1b813054cdd3
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-05-09 02:56:45 +0530

    feat: cleared user data on logout

--- src/components/ProjectListingNavbar.jsx
+++ src/components/ProjectListingNavbar.jsx
@@ -1,60 +1,74 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context";
+import { useAuth, useData } from "../context";
 import { getAuth, signOut } from "firebase/auth";
+
+import { actionTypes } from "../reducers";
 
 const ProjectListingNavbar = () => {
   const { currentUser } = useAuth();
+  const { dataDispatch } = useData();
+  const { CLEAR_DATA } = actionTypes;
   const [showDropdown, setShowDropdown] = useState(false);
   const auth = getAuth();
   const navigate = useNavigate();
-  const signooutHandler = () => {
+  const signoutHandler = () => {
     signOut(auth)
       .then(() => {
+        dataDispatch({ type: CLEAR_DATA });
         navigate("/");
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
   return (
     <nav className="flex justify-between items-center p-8 mb-4">
-      <Link to="/listing/projects">
+      <Link to="/">
         {" "}
         <h1 className="text-2xl font-bold">
           Pro<span className="text-button">Stack</span>
         </h1>
       </Link>
       <div className="flex gap-5">
-        <Link to="/project-form">
-          <button
-            type="button"
-            className="text-buttonText bg-button hover:opacity-90 focus:ring-4  font-medium rounded text-md px-5 py-2.5"
-          >
-            Add project +
-          </button>
-        </Link>
-        <div className="relative">
-          <img
-            className="w-10 h-10 rounded-full"
-            src={currentUser?.avatar}
-            alt=""
-            onClick={() => setShowDropdown(!showDropdown)}
-          />
-          {showDropdown && (
-            <ul className="z-10 absolute -bottom-20 right-0 bg-white shadow-2xl py-4 text-lg text-paragraph font-medium">
-              <Link to={"/userprofile"} onClick={() => setShowDropdown(false)}>
-                <li className="hover:bg-gray-50 px-6">Profile</li>
-              </Link>
-              <li
-                className="hover:bg-gray-50 px-6 text-red-500 cursor-pointer"
-                onClick={signooutHandler}
-              >
-                Signout
-              </li>
-            </ul>
-          )}
-        </div>
+        {currentUser && (
+          <Link to="/project-form">
+            <button
+              type="button"
+              className="text-buttonText bg-button hover:opacity-90 focus:ring-4  font-medium rounded text-md px-5 py-2.5"
+            >
+              Add project +
+            </button>
+          </Link>
+        )}
+        {currentUser && (
+          <div className="relative">
+            <img
+              className="w-10 h-10 rounded-full"
+              src={currentUser?.avatar}
+              alt=""
+              onClick={() => setShowDropdown(!showDropdown)}
+            />
+            {showDropdown && (
+              <ul className="z-10 absolute -bottom-20 right-0 bg-white shadow-2xl py-4 text-md text-paragraph font-medium">
+                <Link
+                  to={"/userprofile"}
+                  onClick={() => setShowDropdown(false)}
+                >
+                  <li className="hover:bg-gray-50 px-6">Profile</li>
+                </Link>
+                <li
+                  className="hover:bg-gray-50 px-6 text-red-500 cursor-pointer"
+                  onClick={signoutHandler}
+                >
+                  {" "}
+                  Logout
+                </li>
+              </ul>
+            )}
+          </div>
+        )}
       </div>
     </nav>
   );
--- src/routes/EndPoints.jsx
+++ src/routes/EndPoints.jsx
@@ -8,18 +8,15 @@ import {
   UserProfile,
 } from "../pages";
 import ErrorPage from "../pages/ErrorPage";
-import PrivateRoute from "./private-route/PrivateRoute";
 
 const EndPoints = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route element={<PrivateRoute />}>
-        <Route path="/listing/projects" element={<ProjectListing />} />
-        <Route path="/listing/users" element={<UserListing />} />
-        <Route path="/project-form" element={<ProjectForm />} />
-        <Route path="/userprofile" element={<UserProfile />} />
-      </Route>
+      <Route path="/listing/projects" element={<ProjectListing />} />
+      <Route path="/listing/users" element={<UserListing />} />
+      <Route path="/project-form" element={<ProjectForm />} />
+      <Route path="/userprofile" element={<UserProfile />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
